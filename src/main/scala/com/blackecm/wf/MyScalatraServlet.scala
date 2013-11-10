426570fce0c60ecdeffa03e6cab648791e676606
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-11-10 10:21:33 -0200

    goodbye, jade. Your presence is no longer needed here.

--- src/main/scala/com/blackecm/wf/MyScalatraServlet.scala
+++ src/main/scala/com/blackecm/wf/MyScalatraServlet.scala
@@ -1,17 +1,11 @@
 package com.blackecm.wf
 
 import org.scalatra._
-import scalate.ScalateSupport
 
 class MyScalatraServlet extends BlackecmWfStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    "hi"
   }
   
 }
