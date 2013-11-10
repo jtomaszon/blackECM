b851161ef64cb22bf21d785e62c2b21a9173ccff
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-11-10 10:38:45 -0200

    cleaning up

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -4,8 +4,6 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    // context.mount(new MyScalatraServlet, "/*")
-
     context.mount(new WorkflowServlet, "/workflows/*")
   }
 }
